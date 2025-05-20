3cf828fdbc537910f7a209d731a779de9b9bf910
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2025-05-19 23:30:18 -0300

    feat: adiconando schema de message

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html')
+def read_root_html():
+    return '<h1>Olá Mundo!</h1>'
